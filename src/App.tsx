732e351d7440ee27bd1240c67335bec84e78f570
Yura <dinourski@gmail.com>
2025-10-16 21:35:49 -0700

    add QueryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { Router } from './Router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <Router />
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   );
 }
